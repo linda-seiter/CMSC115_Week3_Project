40ff81e1428eb7ac36a68ef673f9524b747b0cb5
Linda Seiter <linseiter@gmail.com>
2024-07-17 19:48:43 -0400

    Initial commit

---
+++ PaintCalculator_GuidedProject/Task1 - ceilingArea method/src/PaintCalculator.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+/**
+ * PaintCalculator class calculates the gallons of paint
+ * needed to cover the ceiling and walls of a rectangular room.
+ * @author First Last
+ */
+public class PaintCalculator {
+
+    /**
+     * main - Prompt for room dimensions and print resulting measures.
+     * @param args
+     */
+    public static void main(String[] args) {
+        //TODO Task2-4: Update print statements to call current task's method
+        System.out.println("ceilingArea(12.0, 15.0) = " + ceilingArea(12.0, 15.0));
+        System.out.println("ceilingArea(7.5, 11.5) = " + ceilingArea(7.5, 11.5));
+
+        //TODO Task#5
+
+    }
+
+    /**
+     * ceilingArea - Returns the ceiling area given the room length and width.
+     * @param length - the room length as a double.
+     * @param width - the room width as a double.
+     * @return the ceiling area as a double.
+     */
+    public static double ceilingArea(double length, double width) {
+        return length * width;
+    }
+
+    /**
+     * wallArea - Returns the wall area given the room length, width, and height.
+     * @param length - the room length as a double.
+     * @param width - the room width as a double.
+     * @param height - the room height as a double.
+     * @return the wall area as a double.
+     */
+     //TODO Task#2
+
+
+    /**
+     * roomArea - Returns the total ceiling and wall area given the room length, width, and height.
+     * @param length - the room length as a double.
+     * @param width - the room width as a double.
+     * @param height - the room height as a double.
+     * @return the total ceiling and wall area as a double.
+     */
+    //TODO Task#3
+
+
+    /**
+     * gallonsPaint - Returns the gallons of paint needed to cover the ceiling and walls given the room length, width, and height.
+     * @param length - the room length as a double
+     * @param width - the room width as a double
+     * @param height - the room height as a double
+     * @return the gallons of paint as an int
+     */
+    //TODO Task#4
+
+}
+
